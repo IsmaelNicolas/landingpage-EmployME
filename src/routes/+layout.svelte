2e700792337e1ceb670026bceb4f02912afd753f
Ismael Nicolas Cedillo Paz <ismael.cedillo@outlook.es>
2023-06-11 18:17:08 -0500

    change vite config

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,8 +5,7 @@
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
-	
-	export const prerender = true;
+
 </script>
 
 
